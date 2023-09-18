62d439c349cfe15a42f2a63c05c3dce4154fb73c
phuong.bui <phuong.bui@haegroup.com>
2023-09-18 11:38:04 +0700

    fix google auth

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:86/auth/google/callback',
+        'redirect' => 'http://localhost:88/auth/google/callback',
     ],
 
 ];
